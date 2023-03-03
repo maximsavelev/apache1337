10a66591064b471969fd21a4318a09ef04f25dc5
Maxim <mrmaximax24@yandex.ru>
2023-03-03 23:36:54 +0400

    added repository

---
+++ src/main/java/ru/psuti/apache1337/homeowners/repository/FileStorageRepository.java
@@ -0,0 +1,20 @@
+package ru.psuti.apache1337.homeowners.repository;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public interface FileStorageRepository {
+
+    void init();
+
+    String save(MultipartFile multipartFile);
+
+    Resource load(String fileName);
+
+    Stream<Path> load();
+
+    void clear();
+}
